479293601ae98c361c5042fec362ecb5d2be30fa
Jaidev Bangar <solaxes@gmail.com>
2024-03-30 18:03:39 +0530

    Implemented Shimmer UI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Restaurants from "../data/Restaurants";
+import { API_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 export default function Body() {
   // Local State Variable - Super powerful variable
   // React Hook is a normal javascript function which is given to us, it is prebuilt and comes with some super powers
 
   // State Variable
-  let [listOfRestaurants, setListOfRestaurants] = useState(Restaurants);
+  let [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  /**
+   * useEffect is an another hook available, this has two parameters
+   * First parameter  is call back function and second parameter is dependency array
+   *
+   * useEffect hook is called once the render cycle  is finished
+   *
+   */
+  useEffect(() => {
+    //console.log("useEffect Called");
+    fetchData();
+  }, []);
+
+  // This console log will be called first because, useEffect will execute once the full UI is rendered
+  //console.log("Components rendered");
+
+  const fetchData = async () => {
+    //setListOfRestaurants(Restaurants);
+    // Call API here and set List of fetched restaurants to state variable
+    setListOfRestaurants(Restaurants);
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body-container">
---
+++ src/components/Shimmer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Shimmer() {
+  return (
+    <div class="shimmer-container">
+      <div class="shimmer-card"></div>
+      <div class="shimmer-card"></div>
+      <div class="shimmer-card"></div>
+      <div class="shimmer-card"></div>
+      <div class="shimmer-card"></div>
+    </div>
+  );
+}
